249a67381e3c57a2c94923fcfe6072a96474ddc2
xiao-92 <11865503+xiao-92@users.noreply.github.com>
2022-07-27 13:51:19 +1000

    Create finite_diff.py

---
+++ finite_diff.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 27 13:32:16 2022
+
+@author: henry
+"""
+import numpy as np
+
+def forward_diff(f, x, h):
+    approx = (f(x+h)-f(x)) / h
+    return approx
+
+def backward_diff(f, x, h):
+    approx = (f(x)-f(x-h)) / h
+    return approx
+
+def central_diff(f, x, h):
+    approx = (f(x+h)- f(x-h)) / (2*h)
+    return approx
+
+def func_a(x):
+    return x**3
+
+def func_b(x):
+    return 3*x**2 - 2*x
+
+def func_c(x):
+    return np.sin(x)
